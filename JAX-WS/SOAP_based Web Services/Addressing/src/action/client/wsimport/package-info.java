de3e5a300b7ae75247a24a673fb1e698f905ee19
Adriano <adriano.jesus2@fatec.sp.gov.br>
2017-05-06 12:01:33 -0300

    Using MTOM with altered wsdl

--- JAX-WS/SOAP_based Web Services/Addressing/src/action/client/wsimport/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2005/08/addressing")
-package action.client.wsimport;
--- JAX-WS/SOAP_based Web Services/Addressing/src/action/server/AddNumberException.java
+++
@@ -1,8 +0,0 @@
-package action.server;
-
-public class AddNumberException extends Exception {
-		
-		public AddNumberException(String t1, String t2) {
-			super(t1.concat(t2));
-		}
-}
